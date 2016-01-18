70949cac39f6e7069cd9469bab1fe0f93221e1d5
maibou8888 <1520082546@qq.com>
2016-01-18 16:03:47 +0800

    runtime

--- PanMVVM/viewController/customView/UINavigationItem+MRCBackBarButtonItem.m
+++ PanMVVM/viewController/customView/UINavigationItem+MRCBackBarButtonItem.m
@@ -11,16 +11,6 @@
 
 @implementation UINavigationItem (MRCBackBarButtonItem)
 
-/*
- Method class_getInstanceMethod(Class aClass, SEL aSelector)                返回aClass的名为aSelector的方法
- BOOL class_addMethod(Class cls, SEL name, IMP imp, const char *types)      给cls添加一个新的方法,若干cls存在这个方法则返回失败
- IMP class_replaceMethod(Class cls, SEL name, IMP imp, const char *types)   替换cls的name方法的指针
- void method_exchangeImplementations(Method m1_gen, Method m2_gen)          交换2个方法的实现指针,把父类替的方法替换掉
- 
- IMP method_getImplementation(Method method)    返回method的实现指针
- method_getTypeEncoding(Method m)   返回方法m的参数和返回值的描述的字串
- */
-
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
